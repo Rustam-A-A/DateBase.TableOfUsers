54bd9952038166b2644c450f4495144092eaeb3a
Rustam Aktuganov <aktuganov@yandex.ru>
2021-08-29 15:21:26 +0200

    initial commit

---
+++ TableCreation.sql
@@ -0,0 +1,8 @@
+create table PERSONS (
+    name varchar (20),
+    surname varchar (20),
+    age int,
+    phone_number varchar (20),
+    city_of_living varchar (15),
+    PRIMARY KEY (name, surname, age)
+);
